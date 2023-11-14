09540a7e3efdef0dffa405289b76c1945fd1b931
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-11-14 21:45:23 +0300

    add currency

--- сurrency.js
+++ сurrency.js
@@ -1,18 +1,16 @@
 const buttonExchange = document.querySelector(".button-exchange");
 console.log(buttonExchange);
 const input = document.querySelector(".sum");
-const resultSum = document.querySelector(".result-sum");
-console.log(resultSum);
 const form = document.querySelector(".form");
-const ryRub = document.querySelector(".ryRub");
-console.log(ryRub);
-const pyAed = document.querySelector(".pyAed");
-console.log(pyAed);
 const but = document.querySelectorAll(".but");
 const result = document.querySelectorAll(".result ");
 console.log(result);
-const currensySelector = document.querySelector(".currensy-selector");
-console.log(currensySelector);
+const currensySelectorFrom = document.querySelector(".currensy-selector-from");
+console.log(currensySelectorFrom);
+const currensySelectorTo = document.querySelector(".currensy-selector-to");
+console.log(currensySelectorTo);
+const resultSum = document.querySelector(".result-sum");
+console.log(resultSum);
 
 const convert = async () => {
   const response = await fetch(`https://open.er-api.com/v6/latest/RUB`);
@@ -20,10 +18,8 @@ const convert = async () => {
   console.log(data);
   input.addEventListener("change", (e) => {
     const results = input.value;
-    console.log(
-      (resultSum.textContent =
-        results * data.rates[currensySelector.value].toFixed(2))
-    );
+    resultSum.textContent =
+      results * data.rates[currensySelectorTo.value].toFixed(2);
   });
 };
 convert();
